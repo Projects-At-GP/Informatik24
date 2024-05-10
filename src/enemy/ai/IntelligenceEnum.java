35c0752330c0ccc60bbb6cf7ca618ed7ab53611b
AlbertUnruh <73029826+AlbertUnruh@users.noreply.github.com>
2024-05-10 22:33:30 +0200

    prepare enemy stats (IntelligenceEnum) for combat

--- src/enemy/ai/IntelligenceEnum.java
+++ src/enemy/ai/IntelligenceEnum.java
@@ -1,18 +1,22 @@
 package enemy.ai;
 
 public enum IntelligenceEnum {
-     ABSENT_IQ(0, 0, 0, false),
-     SPIDER(5, 8.5, 3, false);
+    ABSENT_IQ(0, 0, 0, 0, 0, false),
+    SPIDER(5, 8.5, 0.75, 10, 3, false);
 
-     public final double aggressionRange;  // range for initial aggression
-     public final double chasingRange;  // range for chasing
-     public final double speed;  // speed multiplier for movement
-     public final boolean canAlarm;  // can alert other enemies & can be alerted whilst being out of range of alert
+    public final double aggressionRange;  // range for initial aggression
+    public final double chasingRange;  // range for chasing
+    public final double attackRange;  // range to deal damage
+    public final int attackDamage;  // damage to deal
+    public final double speed;  // speed multiplier for movement
+    public final boolean canAlarm;  // can alert other enemies & can be alerted whilst being out of range of alert
 
-     IntelligenceEnum(double aggressionRange, double chasingRange, double speed, boolean canAlarm) {
-         this.aggressionRange = aggressionRange;
-         this.chasingRange = chasingRange;
-         this.speed = speed;
-         this.canAlarm = canAlarm;
-     }
+    IntelligenceEnum(double aggressionRange, double chasingRange, double attackRange, int attackDamage, double speed, boolean canAlarm) {
+        this.aggressionRange = aggressionRange;
+        this.chasingRange = chasingRange;
+        this.attackRange = attackRange;
+        this.attackDamage = attackDamage;
+        this.speed = speed;
+        this.canAlarm = canAlarm;
+    }
 }
