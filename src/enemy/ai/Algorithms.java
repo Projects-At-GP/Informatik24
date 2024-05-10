17e2ab9f8d26705fde6458eec3188edcc4b00e9e
AlbertUnruh <73029826+AlbertUnruh@users.noreply.github.com>
2024-05-10 16:16:59 +0200

    enemies now wander if they don't chase

--- src/enemy/ai/Algorithms.java
+++ src/enemy/ai/Algorithms.java
@@ -5,76 +5,93 @@ import Redfoot.Tile;
 import enemy.ai.Exceptions.NoPathAvailable;
 import vector.Vector2;
 
-import java.util.LinkedList;
-import java.util.LinkedHashMap;
-import java.util.PriorityQueue;
-import java.util.List;
-import java.util.Arrays;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 public class Algorithms {
     private static Renderer.CachedMapData cachedMap;
     private static Vector2 cachedStart;
-    private static Vector2 cachedEnd;
+    private static Vector2 cachedpos;
     private static IntelligenceEnum cachedIntelligence;
     private static LinkedList<Vector2> cachedPath;
 
     /**
      * Checks if the path was already calculated to prevent redundant calculations
      */
-    private static boolean isCached(Renderer.CachedMapData map, Vector2 start, Vector2 end, IntelligenceEnum intelligence) {
+    private static boolean isCached(Renderer.CachedMapData map, Vector2 start, Vector2 pos, IntelligenceEnum intelligence) {
         return (cachedMap == map &&
                 cachedStart == start &&
-                cachedEnd == end &&
+                cachedpos == pos &&
                 cachedIntelligence == intelligence);
     }
 
     /**
      * Sets the cache to reduce redundant calculations
      */
-    private static void setCache(Renderer.CachedMapData map, Vector2 start, Vector2 end, IntelligenceEnum intelligence, LinkedList<Vector2> path) {
+    private static void setCache(Renderer.CachedMapData map, Vector2 start, Vector2 pos, IntelligenceEnum intelligence, LinkedList<Vector2> path) {
         cachedMap = map;
         cachedStart = start;
-        cachedEnd = end;
+        cachedpos = pos;
         cachedIntelligence = intelligence;
         cachedPath = path;
     }
 
     /**
-     * Determines a walkable path from a starting position to an ending position if one is available
+     * Determines a walkable path from a starting position to an posing position if one is available
      * @param map the cached map data containing the chunk with corresponding tiles to determine walkable paths
      * @param start the starting position
-     * @param end the ending position
+     * @param pos the posing position
      * @param intelligence information about the available intelligence
      * @return the walkable path with the first element being the start
      * @throws NoPathAvailable if no path was found throws this exception
      */
-    public static LinkedList<Vector2> getPath(Renderer.CachedMapData map, Vector2 start, Vector2 end, IntelligenceEnum intelligence) throws NoPathAvailable {
-        if (start == null || end == null) return new LinkedList<>();
+    public static LinkedList<Vector2> getPath(Renderer.CachedMapData map, Vector2 start, Vector2 pos, IntelligenceEnum intelligence) throws NoPathAvailable {
+        if (start == null || pos == null) return new LinkedList<>();
 
         // just computed?
-        if (isCached(map, start, end, intelligence)) return cachedPath;
+        if (isCached(map, start, pos, intelligence)) return cachedPath;
 
         // out of range?
-        if (end.subtract(start).magnitude() > intelligence.range) throw new NoPathAvailable(start, end, intelligence);
+        if (pos.subtract(start).magnitude() > intelligence.range) throw new NoPathAvailable(start, pos, intelligence);
 
-        LinkedList<Vector2> path = aStar(map, start, end, (int) Math.pow(intelligence.range, 1.75));  // get detailed path
+        LinkedList<Vector2> path = aStar(map, start, pos, (int) Math.pow(intelligence.range, 1.75));  // get detailed path
 
-        setCache(map, start, end, intelligence, path);
+        setCache(map, start, pos, intelligence, path);
         return path;
+    }
+
+    /**
+     * Helper method to retrieve all walkable neighbors from a given position
+     * @param map the cached map data
+     * @param pos the current position
+     * @return an array of all available/walkable neighbors
+     */
+    public static Vector2[] getWalkableNeighbors(Renderer.CachedMapData map, Vector2 pos) {
+        LinkedList<Vector2> possibleNeighbors = new LinkedList<>(List.of(
+                new Vector2(pos.x+1, pos.y),
+                new Vector2(pos.x-1, pos.y),
+                new Vector2(pos.x, pos.y+1),
+                new Vector2(pos.x, pos.y-1))
+        );
+        return Arrays.stream(map.mapData).flatMap(Arrays::stream)
+                                         .filter(tile -> possibleNeighbors.contains(tile.pos))
+                                         .filter(Tile::isWalkable)
+                                         .map(tile -> tile.pos)
+                                         .toArray(Vector2[]::new);
     }
 
     /**
      * Implementation based on pseudocode from <a href="https://en.wikipedia.org/wiki/A*_search_algorithm#Pseudocode">Wikipedia <i>(A_Star)</i></a>
      * @param map the cached map data containing the chunk with corresponding tiles to determine walkable paths
      * @param start the starting position
-     * @param end the ending position
+     * @param pos the posing position
      * @return the walkable path with the first element being the start
      * @throws NoPathAvailable if no path was found throws this exception
      */
-    private static LinkedList<Vector2> aStar(Renderer.CachedMapData map, Vector2 start, Vector2 end, int maxIterations) throws NoPathAvailable {
+    private static LinkedList<Vector2> aStar(Renderer.CachedMapData map, Vector2 start, Vector2 pos, int maxIterations) throws NoPathAvailable {
         start = new Vector2(Math.round(start.x), Math.round(start.y));
-        end = new Vector2(Math.round(end.x), Math.round(end.y));
+        pos = new Vector2(Math.round(pos.x), Math.round(pos.y));
 
         int iterations = 0;
 
@@ -87,7 +104,7 @@ public class Algorithms {
 
         // fScore
         LinkedHashMap<Vector2, Double> fScore = new LinkedHashMap<>();
-        fScore.put(start, calculateCost(start, end));
+        fScore.put(start, calculateCost(start, pos));
 
         PriorityQueue<Vector2> openSet = new PriorityQueue<>((o1, o2) -> (int) ((fScore.get(o1) - fScore.get(o2)) * 100));
         openSet.offer(start);
@@ -96,36 +113,24 @@ public class Algorithms {
         double tentativeGScore;
         while (!openSet.isEmpty()) {
             // cap the calculations
-            if (iterations > maxIterations) throw new NoPathAvailable(start, end);
+            if (iterations > maxIterations) throw new NoPathAvailable(start, pos);
 
             current = openSet.poll();
-            if (current.equals(end)) return reconstructAStarPath(cameFrom, current);
+            if (current.equals(pos)) return reconstructAStarPath(cameFrom, current);
 
-            Vector2 finalCurrent = current;  // good practice to make a somewhat final version for lambdas
-            LinkedList<Vector2> possibleNeighbors = new LinkedList<>(List.of(
-                    new Vector2(finalCurrent.x+1, finalCurrent.y),
-                    new Vector2(finalCurrent.x-1, finalCurrent.y),
-                    new Vector2(finalCurrent.x, finalCurrent.y+1),
-                    new Vector2(finalCurrent.x, finalCurrent.y-1))
-            );
-            Vector2[] neighbors = Arrays.stream(map.mapData).flatMap(Arrays::stream)
-                                                            .filter(tile -> possibleNeighbors.contains(tile.pos))
-                                                            .filter(Tile::isWalkable)
-                                                            .map(tile -> tile.pos)
-                                                            .toArray(Vector2[]::new);
-            for (Vector2 neighbor : neighbors) {
+            for (Vector2 neighbor : getWalkableNeighbors(map, current)) {
                 tentativeGScore = gScore.get(current) + 1;  // 1 is default value for walking up/down/left/right; no diagonals here
                 if (tentativeGScore < gScore.getOrDefault(neighbor, Double.MAX_VALUE)) {
                     cameFrom.put(neighbor, current);
                     gScore.put(neighbor, tentativeGScore);
-                    fScore.put(neighbor, tentativeGScore + calculateCost(neighbor, end));
+                    fScore.put(neighbor, tentativeGScore + calculateCost(neighbor, pos));
                     if (!openSet.contains(neighbor)) openSet.add(neighbor);
                 }
             }
             iterations++;
         }
 
-        throw new NoPathAvailable(start, end);
+        throw new NoPathAvailable(start, pos);
     }
 
     /**
@@ -150,5 +155,19 @@ public class Algorithms {
      */
     private static Double calculateCost(Vector2 current, Vector2 target) {
         return current.subtract(target).magnitude();
+    }
+
+    /**
+     * Retrieves a random direction to wander to
+     * @param map the cached map data
+     * @param pos the current position
+     * @return a random (available) direction
+     */
+    public static Vector2 getWanderInstruction(Renderer.CachedMapData map, Vector2 pos) {
+        if (pos == null) return null;
+        pos = new Vector2(Math.round(pos.x), Math.round(pos.y));
+        Vector2[] neighbors = getWalkableNeighbors(map, pos);
+        if (neighbors.length == 0) return pos;
+        return neighbors[ThreadLocalRandom.current().nextInt(neighbors.length) % neighbors.length].subtract(pos);
     }
 }
